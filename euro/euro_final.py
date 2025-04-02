08562ede7394b94f7a372fdb24c09e72d13532c6
Jacob Colchamiro <jacob.colchamiro@gmail.com>
2025-04-02 10:32:25 -0400

    f

--- euro/euro_final.py
+++ euro/euro_final.py
@@ -101,16 +101,16 @@ X_init[:, 2] = t_init
 
 idx = int(os.environ["SLURM_ARRAY_TASK_ID"])
 
-configs = [nn_configs[idx]]
+configs = [pinn_configs[idx]]
 
-ffnn = euro_model_train.train_pinn(False, X_train, y_train, X_val, y_val,
+pinn = euro_model_train.train_pinn(True, X_train, y_train, X_val, y_val,
                               X_PDE, X_bound_1, X_bound_2, X_init, 128, means, stds, configs,
                              seed=seed)
 X_joint = np.concatenate((X_train, X_val), axis=0)
 
 y_joint = np.concatenate((y_train, y_val), axis=0)
-final_loss = tf.reduce_mean(tf.square(tf.reshape(ffnn(X_joint), [-1]) - tf.cast(y_joint, tf.float32)))
-ffnn.save(f"saved_models2/ffnn_model_{configs[0]}.h5")
+final_loss = tf.reduce_mean(tf.square(tf.reshape(pinn(X_joint), [-1]) - tf.cast(y_joint, tf.float32)))
+pinn.save(f"saved_models2/pinn_model_{configs[0]}.h5")
 
 df = pd.DataFrame([{"config": configs[0], "final_loss": final_loss}])
 df.to_csv(f"saved_models2/loss_{configs[0]}.csv", index=False)
